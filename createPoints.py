421acbd6fe405b76283690a9bcbff43d30564832
Χρήστος Πατρινόπουλος <christpa98@windowslive.com>
2024-04-16 19:41:42 +0300

    Optimize datapoints script

--- createPoints.py
+++ createPoints.py
@@ -2,17 +2,19 @@ import os
 import random, math, matplotlib.pyplot as plt
 
 def create_points(centers, num_points):
-    n_cluster1 = (0, num_points // 3)
-    n_cluster2 = (n_cluster1[1], num_points // 3 * 2)
-    n_cluster3 = (n_cluster2[1], num_points // 3 * 3)
-    n_cluster = [n_cluster1, n_cluster2, n_cluster3]
-    # print(len(range(n_cluster1[0], n_cluster1[1]))==len(range(n_cluster2[0], n_cluster2[1]))==len(range(n_cluster3[0], n_cluster3[1])))
-    # print(n_cluster1, n_cluster2, n_cluster3)
-    points = []
+
+    n_clusters = []
+    n_points = 0
+    batch_size = num_points // len(centers)
+    for c in range(len(centers)):
+        n_clusters.append((n_points, n_points + batch_size))
+        n_points += batch_size
     
-    for n_cl in n_cluster:
+
+    points = []
+    for n_cl in n_clusters:
         for i in range(n_cl[0], n_cl[1]):
-            center = centers[0]
+            center = centers[n_clusters.index(n_cl)]
             distance = random.expovariate(0.05)
             angle = random.uniform(0, 2 * math.pi)
 
@@ -21,30 +23,27 @@ def create_points(centers, num_points):
 
             points.append((x, y))
 
-        centers = centers[1:]
 
     return points
 
 def write_to_file(points, filename):
     path = os.getcwd() + '/input/' + filename
+    #path = os.getcwd() + '/' + filename
     with open(path, 'w') as f:
         for point in points:
             f.write(f'{point[0]} {point[1]}\n')
 
 if __name__ == '__main__':
 
-    centers = [(-100, 200), (300, 300), (0, -100)]
-    num_points = 10**6+3
+    centers = [(-100, 200), (300, 300), (0, -100)]  # centers of clusters
+    num_points = 10**6+3                            # number of points
 
-    points = create_points(centers, num_points)
-    write_to_file(points, 'datapoints.txt')
+    points = create_points(centers, num_points)     # create points
+    write_to_file(points, 'datapoints.txt')         # write points to file
 
-    print(points[:10])
-    plt.scatter(*zip(*points))
-    plt.plot(*zip(*centers), 'ro')
-    plt.show()
+    print(f'Points created: {len(points)} \n')
+    print(points[:10])                              # print first 10 points
 
-
-
-
-        
+    plt.scatter(*zip(*points))                      # plot points
+    plt.plot(*zip(*centers), 'ro')                  # plot centers
+    plt.show()                                      # show plot
